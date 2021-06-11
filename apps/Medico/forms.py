67244c2f89ba79c658a91eb641b3842c2582ce86
MF Munoz <ma.munozleppez@gmail.com>
2021-06-11 01:41:44 -0400

    se sube proyecto clinica

---
+++ apps/Medico/forms.py
@@ -0,0 +1,29 @@
+
+from django import forms
+from .models import CitaMedica
+class CitaMedicaForm(forms.ModelForm): 
+    class Meta:  
+        model = CitaMedica
+        fields = ['run', 'nombre_paciente','tipo_prevision', 'especialidad', 'medico', 'fecha_cita', 'hora_cita'
+                  , 'email'] 
+
+        labels = {
+            'run': 'Run',
+            'nombre_paciente': 'Nombre paciente',
+            'tipo_prevision': 'Prevision',
+            'especialidad': 'Especialidad',
+            'medico': 'Nombre medico',
+            'fecha_cita': 'Fecha Cita',
+            'hora_cita': 'Hora Cita',
+            'email' : 'Email',
+        }
+        widgets = {
+            'run': forms.TextInput(attrs={'class': 'form-control'}), 
+            'nombre_paciente': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo_prevision': forms.Select(attrs={'class':'form-control'}),
+            'especialidad': forms.Select(attrs={'class':'form-control'}),
+            'medico': forms.Select(attrs={'class':'form-control', 'disabled' : 'disabled'}),
+            'fecha_cita' : forms.TextInput(attrs={'class': 'form-control', 'readonly' : 'readonly'}),
+            'hora_cita' : forms.TextInput(attrs={'class': 'timepicker'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}), 
+        }
---
+++ apps/Medico/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+class Especialidad(models.Model):
+    nombre_especialidad = models.TextField()
+    valor = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_especialidad
+
+class Medico(models.Model):
+    run_medico = models.TextField(primary_key=True)
+    nombre_medico = models.TextField()
+    especialidad = models.ForeignKey(Especialidad, null=True, blank=True, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nombre_medico
+    
+
+TIPO_PREVISION = (
+    ('Fonasa', 'Fonasa'),
+    ('Isapre', 'Isapre'),
+    ('Ninguna', 'Ninguna'),
+)
+
+
+class CitaMedica(models.Model):
+    run= models.TextField(primary_key=True)
+    nombre_paciente = models.TextField()
+    tipo_prevision = models.CharField(max_length=50, choices=TIPO_PREVISION)
+    medico = models.ForeignKey(Medico, related_name='medico', null=True, blank=True, on_delete=models.CASCADE)
+    fecha_cita =  models.DateField()
+    hora_cita = models.TimeField(blank=True, null=True)
+    email = models.EmailField(max_length=70,blank=True, null= True)
+    especialidad = models.ForeignKey(Especialidad, related_name='especialidad', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_paciente
+
+
+
